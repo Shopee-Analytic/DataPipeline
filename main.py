d98b3c45b80a998b8e50eb81eb60cee88f3378ba
lthoangg <letronghoang00@gmail.com>
2021-04-14 15:52:59 +0700

    Add handle if folder 'data' or 'config' not found

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tools.crawler import crawl
 from tools.visualizer import visualize
 import sys
 import yaml
+from os import path, mkdir, getcwd
 
 def is_int(val):
     try:
@@ -16,14 +17,27 @@ def load_config(file):
         return links
 
 if __name__ == "__main__":
+    try:
+        assert path.exists('config')
+    except:
+        print("Folder config not existed")
+        mkdir(path.join(getcwd(), 'config'))
+        print("config is created.\n")
+    try:
+        assert path.exists('data')
+    except:
+        print("Folder data not existed")
+        mkdir(path.join(getcwd(), 'data'))
+        print("data is created.\n")
+
     assert sys.argv[1] in ("crawl", "visualize"), '"crawl" or "visualize" should be call'
 
     if sys.argv[1] == "crawl":
         assert sys.argv[2] in ("--input", "-I"), 'Should be "--input" or "-I".'
         assert sys.argv[3].endswith(('.yml', ".yaml")) , "File should named by 'file_name.yml'."
+        assert path.exists('config/{}'.format(sys.argv[3])), "File not existed"
         assert sys.argv[4] in ("--ouput", "-O"), 'Should be "--output" or "-0".'
         assert sys.argv[5].endswith(".json"), 'File should be named by "file_name.json".'
-
         links = load_config(sys.argv[3])
         assert links != None, f"No key in {sys.argv[3]}."
         for link in links:
@@ -37,7 +51,14 @@ if __name__ == "__main__":
     elif sys.argv[1] == "visualize":
         assert sys.argv[2] in ("--file", "-F"), 'Should be "--file" or "-F".'
         assert sys.argv[3].endswith(".json"), 'File should be named by "file_name.json".'
-        assert is_int(sys.argv[4]), "Number of product should be an integer"
-        
-        visualize(sys.argv[3], sys.argv[4])
+        assert path.exists('data/{}'.format(sys.argv[3]))
+        try:
+            assert is_int(sys.argv[4]), "Number of product should be an integer"
+            length = int(sys.argv[4])
+        except AssertionError as msg:
+            print(msg)
+        except IndexError:
+            length=1
+
+        visualize(sys.argv[3], length)
             
